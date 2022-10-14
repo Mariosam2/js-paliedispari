9fe0f675ef5ff37a74f8e0cdc8b7df20cd3bfeea
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-14 12:23:09 +0200

    cannot use charAt, changed string methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,50 @@
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma (parola identica se letta da entrambi i lati)
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-// Prendo la parola come stringa dall'utente la leggo da destra verso sinistra usando charAt
-// in un loop "al contrario", salvo la stringa e la confronto con la stringa data dall'utente
+// Prendo la parola come stringa dall'utente la leggo da destra verso sinistra usando charAt in un loop "al contrario", salvo la stringa e la confronto con la stringa data dall'utente
+
 
 // charAt(position)
+// concat(char)
 //prompt
 //function
 //for loop
 
-const userWord = prompt('Inserisci una parola');
-
-// creazione dei metodi
-function isPalindrome(string) {
-    let rightToLeft;
-    // percorro la stringa da destra verso sinistra
-    for(let i = string.length; i > 0; i--){
-        rightToLeft.concat(string.charAt(i));
+/* const userWord = prompt('Inserisci una parola'); */
+//test array
+const testsArray = [
+    'Acca',
+    'Ada',
+    'Kayak',
+    'Palindromo',
+    'Test',
+    'Idi',
+    'Pop',
+    'Ingegni',
+    ' ',
+]
+test(testsArray);
+//test method
+function test (stringsArray){
+    for(let i = 0; i < stringsArray.length; i++){
+        //console.log(stringsArray[i]);
+        isPalindrome(stringsArray[i]);
     }
+}
+// creazione dei metodi
+function isPalindrome(word) {
+    word = word.toLocaleLowerCase();
+    let rightToLeftWord = '';
+    //console.log(word);
+    // percorro la stringa da destra verso sinistra
+    for(let i = word.length - 1; i >= 0; i--){
+        console.log(word[i].toString());
+        rightToLeftWord += word[i];
+    }
+    word.trim();
+    rightToLeftWord.trim();
 
-    if(string === rightToLeft){
+    if(word === rightToLeftWord){
         console.log('is palindrome');
     } else {
         console.log('is not palindrome');
